08033f65d621056dcd4fbf1390c8893dfd84fa48
developerweb510 <124876701+luanavallejos@users.noreply.github.com>
2023-04-19 21:33:37 +0530

    äpi intgratio

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,32 +1,31 @@
 import { event } from "jquery";
 import React, { useState } from "react";
 // react-bootstrap components
-import {
-  Button,
-  Card,
-  Container,
-  Row,
-  Col,
-  Form
-} from "react-bootstrap";
+import { Button, Card, Container, Row, Col, Form } from "react-bootstrap";
+import Validation from "./validations/login-validations";
+import axios from "axios";
 
 function Login() {
-  const[ email, setEmail] =useState("");
-  const[ password, setPassword] =useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const [errors, setError] = useState({});
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    let item = {email, password}
-    axios.post('http://localhost/3d-backend/api/login', item)
-    .then(res=> console.log(res))
-  }
+    let item = { email, password };
+    setError(Validation(item));
+    axios
+      .post("http://localhost/3d-backend/api/login", item)
+      .then((res) => console.log(res));
+  };
   return (
     <>
       <Container fluid>
         <Row>
           <Col md=""></Col>
-          <Col md="8">
-            <Card style={{ width: '33rem' }}>
+          <Col md="6">
+            <Card>
               <Card.Header>
                 <div className="text-center">
                   <p>Login Form</p>
@@ -38,44 +37,60 @@ function Login() {
                   <Form action="" onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                       <Form.Label>User Email </Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" name="email" value={email}  onChange={(e)=>setEmail(e.target.value)} />
+                      <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      {errors.email && <p> {errors.email} </p>}
                       <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                       </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                       <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" name="password" value={password}  onChange={(e)=>setPassword(e.target.value)} />
+                      <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      {errors.password && <p> {errors.password} </p>}
                     </Form.Group>
                     <Form.Group className="d-flex mb-3" id="formGridCheckbox">
                       <Form.Check type="checkbox" label="Remember me" />
-                      <a href="#" className="ml-auto">Forgot password?</a>
+                      <a href="#" className="ml-auto">
+                        Forgot password?
+                      </a>
                     </Form.Group>
-                    <Button className="btn-fill form-control"
+                    <Button
+                      className="btn-fill form-control"
                       type="submit"
-                      variant="primary">Login</Button>
-                    
+                      variant="primary"
+                    >
+                      Login
+                    </Button>
                   </Form>
                   <div className="text-center mt-4">
-                      <p>Don't have an account ?-<a href="/Login" className="text-primary">Sign Up</a></p>
-                    </div>
+                    <p>
+                      Don't have an account ?-
+                      <a href="/Login" className="text-primary">
+                        Sign Up
+                      </a>
+                    </p>
+                  </div>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col md=""></Col>
         </Row>
-
-
-
-
       </Container>
     </>
   );
 }
 
 export default Login;
-
-
-
-
--- src/views/Register.js
+++ src/views/Register.js
@@ -1,114 +1,122 @@
 import axios from "axios";
-import React, {useState } from "react";
+import React, { useState } from "react";
 // react-bootstrap components
-import {
-  Button,
-  Card,
-  Container,
-  Row,
-  Col,
-  Form,
-  
-} from "react-bootstrap";
+import { Button, Card, Container, Row, Col, Form } from "react-bootstrap";
+import RValidation from "./validations/register-validation";
 
+function FormFloatingRegister() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [c_password, setCPassword] = useState("");
 
-function  FormFloatingRegister() {
-
-  // const [values, setValues] = useState({
-  //   name:'',
-  //   email:'',
-  //   password:'',
-  //   c_password:'',
-  // })
-
-  const[ name, setName] =useState("");
-  const[ email, setEmail] =useState("");
-  const[ password, setPassword] =useState("");
-  const[ c_password, setCPassword] =useState("");
-
-  // const handleInput =(event) => {
-  //   setValues(prev => ({...prev, [event.target.name] : [event.target.value]}))
-  // }
+  const [errors, setError] = useState({});
 
   const handleSubmit = (event) => {
-    let item = {name, email, password, c_password}
+    let item = { name, email, password, c_password };
     event.preventDefault();
     console.log(item);
-    
-    axios.post('http://localhost/3d-backend/api/register', item )
-    .then(res=> console.log(res))
-    
-  }
+    setError(RValidation(item));
+
+    axios
+      .post("http://localhost/3d-backend/api/register", item)
+      .then((res) => console.log(res));
+  };
   return (
     <>
       <Container fluid>
-       <Row>
-        <Col md=""></Col>
-        <Col md="8">
-        <Card  style={{ width: '30rem' }}>
-        <Card.Header>
-          <div className="text-center">
-              <p>Register Form</p>
-          </div>
-          </Card.Header>
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-          <Form action="" onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" name="name" value={name}  onChange={(e)=>setName(e.target.value)}  placeholder="Enter name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" name="email" value={email}  onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" name="password" value={password}  onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" name="c_password" value={c_password}  onChange={(e)=>setCPassword(e.target.value)} placeholder="Confirm Password" />
-                </Form.Group>
-                <Form.Group className="d-flex mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Remember me"  />
-                  <a href="#" className="ml-auto">Forgot password?</a>
-                </Form.Group>
-              <Button className="btn-fill form-control" type="submit"
-                    variant="primary" >
-                Sign Up
-              </Button>
-              <div className="text-center mt-4">
-                <p>You have an account ?-<a href="/Login"  className="text-primary">Login</a></p>
-              </div>
-          </Form>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-        </Col>
-        <Col md=""></Col>
-       </Row>
-       
+        <Row>
+          <Col md=""></Col>
+          <Col md="6">
+            <Card>
+              <Card.Header>
+                <div className="text-center">
+                  <p>Register Form</p>
+                </div>
+              </Card.Header>
+              <Card.Body>
+                <Card.Title></Card.Title>
+                <Card.Text>
+                  <Form action="" onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter name"
+                      />
+                      {errors.name && <p> {errors.name} </p>}
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter email"
+                      />
+                      {errors.email && <p> {errors.email} </p>}
+                      <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                      </Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                      />
+                      {errors.password && <p> {errors.password} </p>}
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Confirm Password</Form.Label>
+                      <Form.Control
+                        type="password"
+                        name="c_password"
+                        value={c_password}
+                        onChange={(e) => setCPassword(e.target.value)}
+                        placeholder="Confirm Password"
+                      />
+                      {errors.c_password && <p> {errors.c_password} </p>}
+                    </Form.Group>
+                    <Form.Group className="d-flex mb-3" id="formGridCheckbox">
+                      <Form.Check type="checkbox" label="Remember me" />
+                      <a href="#" className="ml-auto">
+                        Forgot password?
+                      </a>
+                    </Form.Group>
+                    <Button
+                      className="btn-fill form-control"
+                      type="submit"
+                      variant="primary"
+                    >
+                      {" "}
+                      Sign Up
+                    </Button>
+                    <div className="text-center mt-4">
+                      <p>
+                        You have an account ?-
+                        <a href="/Login" className="text-primary">
+                          Login
+                        </a>
+                      </p>
+                    </div>
+                  </Form>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md=""></Col>
+        </Row>
       </Container>
     </>
   );
 }
 
-export default  FormFloatingRegister;
-
-
-
-
-// import React from 'react';
-// import { MDBInput } from 'mdb-react-ui-kit';
-// function Register() {
-//   return (
-//     <MDBInput label='Example label' id='form1' type='text' />
-//   );
-// }
-
-// export default Register;
+export default FormFloatingRegister;
